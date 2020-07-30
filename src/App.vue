ae2b81d9e6cfc69c5930d54b169030757f0d8d6d
BlackPelican <BlackPelican@github.com>
2020-07-30 15:20:02 +0200

    added css resets

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,44 @@ export default {
 </script>
 
 <style>
+/*---Resets---*/
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
+button,
+input,
+button:focus,
+input:focus {
+  outline: none;
+  border: 1px solid black;
+  font-size: 1rem;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  text-decoration: none;
+}
+
+::selection {
+  background: var(--secondary);
+  color: var(--black);
+}
+
 html,
 body {
   position: relative;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   font-family: "Open Sans";
 }
 
+/*---vars---*/
 :root {
   --primary: #6200ee;
   --primary-variant: #3700b3;
